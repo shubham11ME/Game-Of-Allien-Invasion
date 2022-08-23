0af30ebc6857999088bb86e9d4a2a1109d08605f
Shubham SIngh Chauhan <74720483+shubham11ME@users.noreply.github.com>
2022-08-23 13:22:44 +0530

    Add files via upload

---
+++ button.py
@@ -0,0 +1,33 @@
+import pygame.font # render text to the screen.
+
+class Button:
+    """Create a start button for the alien invasion"""
+
+    def __init__(self, ai_game, msg):
+        """Initialize button attributes."""
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+
+        # Set the dimension and properties of button.
+        self.width, self.height = 200, 50
+        self.button_color = (0, 255, 0)
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.SysFont(None, 48) # None argument tells pygame to use the default font and size of 48
+
+        # Build the button rect's object and center it
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.center = self.screen_rect.center
+
+        # The button message needs to prepared only once.
+        self._prep_msg(msg)
+
+    def _prep_msg(self, msg):
+        """Turn msg into a rendered image and center text on the button."""
+        self.msg_image = self.font.render(msg, True, self.text_color, self.button_color) # font.render() turns the text stored in msg into an image. Boolean value to turn antialiasing on or off
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+
+    def draw_button(self):
+        """Draw blank button and then draw message."""
+        self.screen.fill(self.button_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
