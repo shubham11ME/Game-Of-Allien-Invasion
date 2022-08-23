f01dc55a84f7191e080fa7a48e4348b6899048d8
Shubham SIngh Chauhan <74720483+shubham11ME@users.noreply.github.com>
2022-08-23 13:17:17 +0530

    Update alien.py

--- alien.py
+++ alien.py
@@ -8,6 +8,7 @@ class Alien(Sprite):
         """Initialize the alien and set starting position."""
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         # upload the alien image and set its rect attributes.
         self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
@@ -18,3 +19,14 @@ class Alien(Sprite):
 
         # Store the alien exact horizontal position.
         self.x = float(self.rect.x)
+
+    def update(self):
+        """Move the alien to the right."""
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
+        self.rect.x = self.x
+
+    def check_edges(self):
+        """Return True if an alien is at edge of screen."""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
